f84b34beebf9351ce7cd4b1e4d0f1aed2b1d0998
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-07-02 12:57:36 -0500

    feat: user can see the home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/todoisnt-logo.png";
+import plus from "./images/icons/plus.svg";
+import "./index.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img src={logo} className="header__logo" alt="todoisnt-logo" />
       </header>
+      <main className="main">
+        <h1 className="main__title">Todos</h1>
+        <button class="add-task-button">
+          <img src={plus} alt="add-task" />
+          Add Task
+        </button>
+        <button class="show-completed-button">Show completed</button>
+      </main>
     </div>
   );
 }
