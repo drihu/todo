17cf928099f3379f8bfef4fd35165d953c851506
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-07-02 15:24:59 -0500

    feat: user can add a task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "./images/todoisnt-logo.png";
 import plus from "./images/icons/plus.svg";
+import checkbox from "./images/icons/checkbox.svg";
 import "./index.css";
 import "./App.css";
 
@@ -14,11 +15,13 @@ function AddTaskButton({ onClick }) {
 
 function CreateForm({ onSubmit, onCancel, placeholder }) {
   return (
-    <form className="create-form" onSubmit={onSubmit}>
+    <form className="create-form" onSubmit={onSubmit} noValidate>
       <input
         className="create-form__input"
         type="text"
+        name="input"
         placeholder={placeholder}
+        autoComplete="off"
       />
       <button type="submit" className="create-form__add-button">
         Add
@@ -30,12 +33,28 @@ function CreateForm({ onSubmit, onCancel, placeholder }) {
   );
 }
 
+function Task({ task }) {
+  return (
+    <article className="task">
+      <img src={checkbox} className="task__checkbox" alt="task-radio" />
+      {task}
+    </article>
+  );
+}
+
 function App() {
   const [isCreateFormActive, setIsCreateFormActive] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
   const submitCreateForm = (event) => {
     event.preventDefault();
-    console.log('submit create form');
+    const taskname = event.target.input.value;
+    if (taskname.length < 3) {
+      alert("The task should be at least 3 characters long");
+      return;
+    }
+    setTasks([...tasks, taskname]);
+    event.target.input.value = "";
   };
 
   return (
@@ -43,8 +62,16 @@ function App() {
       <header className="header">
         <img src={logo} className="header__logo" alt="todoisnt-logo" />
       </header>
+
       <main className="main">
         <h1 className="main__title">Todos</h1>
+
+        <section className="main__section">
+          {tasks
+            ? tasks.map((task, index) => <Task task={task} key={index} />)
+            : null}
+        </section>
+
         <section className="main__section">
           {!isCreateFormActive ? (
             <AddTaskButton onClick={() => setIsCreateFormActive(true)} />
@@ -56,7 +83,8 @@ function App() {
             />
           )}
         </section>
-        <section className="main__section">
+
+        <section className="main__section show-completed-button-container">
           <button className="show-completed-button">Show completed</button>
         </section>
       </main>
