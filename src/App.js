5220bd076c15bab70d0652e71570adcd949472fe
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-07-02 14:29:14 -0500

    feat: user can see a form to add a task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./images/todoisnt-logo.png";
 import plus from "./images/icons/plus.svg";
 import "./index.css";
 import "./App.css";
 
+function AddTaskButton({ onClick }) {
+  return (
+    <button onClick={onClick} className="add-task-button">
+      <img src={plus} alt="add-task" /> Add Task
+    </button>
+  );
+}
+
+function CreateForm({ onSubmit, onCancel, placeholder }) {
+  return (
+    <form className="create-form" onSubmit={onSubmit}>
+      <input
+        className="create-form__input"
+        type="text"
+        placeholder={placeholder}
+      />
+      <button type="submit" className="create-form__add-button">
+        Add
+      </button>
+      <button className="create-form__cancel-button" onClick={onCancel}>
+        Cancel
+      </button>
+    </form>
+  );
+}
+
 function App() {
+  const [isCreateFormActive, setIsCreateFormActive] = useState(false);
+
+  const submitCreateForm = (event) => {
+    event.preventDefault();
+    console.log('submit create form');
+  };
+
   return (
     <div className="app">
       <header className="header">
@@ -12,11 +45,20 @@ function App() {
       </header>
       <main className="main">
         <h1 className="main__title">Todos</h1>
-        <button class="add-task-button">
-          <img src={plus} alt="add-task" />
-          Add Task
-        </button>
-        <button class="show-completed-button">Show completed</button>
+        <section className="main__section">
+          {!isCreateFormActive ? (
+            <AddTaskButton onClick={() => setIsCreateFormActive(true)} />
+          ) : (
+            <CreateForm
+              onSubmit={submitCreateForm}
+              onCancel={() => setIsCreateFormActive(false)}
+              placeholder={"Write a todo..."}
+            />
+          )}
+        </section>
+        <section className="main__section">
+          <button className="show-completed-button">Show completed</button>
+        </section>
       </main>
     </div>
   );
